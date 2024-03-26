b2d31a5916542d781ef1eef020b0661cc7ebe3d0
nu11zy <55335158+nu11zy@users.noreply.github.com>
2024-03-26 19:05:02 +0300

    Update xfce.nix

--- system/wm/xfce/xfce.nix
+++ system/wm/xfce/xfce.nix
@@ -1,1 +1,14 @@
-#
+{ pkgs, ... }:
+
+{
+  imports = [
+    ../x11.nix
+  ];
+
+  services.xserver.displayManager = {
+    lightdm.enable = true;
+    defaultSession = "xfce";
+  };
+
+  services.xserver.desktopManager.xfce.enable = true;
+}
