71f830b1539a64638aaf146ea956de0003d3bcb7
nu11zy <55335158+nu11zy@users.noreply.github.com>
2024-03-26 19:15:40 +0300

    Update home.nix

--- profile/void/home.nix
+++ profile/void/home.nix
@@ -1,1 +1,20 @@
-# home
+{ pkgs, customSettings, ... }:
+
+{
+  home.username = customSettings.username;
+  home.homeDirectory = "/home/"+customSettings.username;
+
+  programs.home-manager.enable = true;
+
+  imports = [
+    ../../user/shell/zsh.nix
+    ../../user/app/chromium.nix
+    ../../user/terminal/alacritty.nix
+  ];
+
+  home.stateVersion = "22.11";
+
+  home.packages = with pkgs; [
+    vlc
+  ];
+}
