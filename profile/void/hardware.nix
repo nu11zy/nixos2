d706fc0a7e3a84888c0b15d4aa14236d96824ec9
nu11zy <55335158+nu11zy@users.noreply.github.com>
2024-03-26 18:41:15 +0300

    Update hardware.nix

--- profile/void/hardware.nix
+++ profile/void/hardware.nix
@@ -1,1 +1,16 @@
-# todo
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+    # Nvidia drivers
+    services.xserver.videoDrivers = ["nvidia"];
+    hardware.nvidia = {
+        modesetting.enable = true;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+    hardware.opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true;
+    };
+}
